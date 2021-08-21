36f24ff41e9fffbddc5225cb1fbaeba5071c514e
yalcinPXL <yalcinPXL@gmail.com>
2021-08-21 22:04:38 +0200

    Add project files.

---
+++ SqlTableDependencyClient/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlTableDependencyClient
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        //Properties not present in the database are ignored
+        public string City { get; set; }
+        public DateTime Born { get; set; }
+    }
+}
---
+++ SqlTableDependencyClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.Enums;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace SqlTableDependencyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = @"Data Source=PF1GGK43\SQLEXPRESS02;Initial Catalog=SqlTableDependency;MultipleActiveResultSets=true; Integrated Security=true;";
+
+            using (var tableDependency=new SqlTableDependency<Customer>(cs,"Customer"))
+            {
+                tableDependency.OnChanged += TableDependency_OnChanged;
+                tableDependency.OnError += TableDependency_OnError;
+
+                tableDependency.Start();
+                Console.WriteLine("Waiting for receiving notifications...");
+                Console.WriteLine("Press a key to stop.");
+                Console.ReadLine();
+                tableDependency.Stop();
+            } 
+        }
+
+        private static void TableDependency_OnChanged(object sender, RecordChangedEventArgs<Customer> e)
+        {
+            Console.WriteLine(Environment.NewLine);
+            if (e.ChangeType != ChangeType.None)
+            {
+               
+                var changedEntity = e.Entity;
+                Console.WriteLine("DML operatios: " + e.ChangeType);
+                Console.WriteLine("ID: " + changedEntity.Id);
+                Console.WriteLine("Name: " + changedEntity.Name);
+                Console.WriteLine("SurName: " + changedEntity.Surname);
+                Console.WriteLine(Environment.NewLine);
+
+            }     
+        }
+
+        private static void TableDependency_OnError(object sender, ErrorEventArgs e)
+        {
+            Exception ex = e.Error;
+            throw ex;
+        }
+    }
+}
